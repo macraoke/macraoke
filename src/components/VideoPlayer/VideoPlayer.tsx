bee84c3aee3e94d958a689e6ad633d98ddbcba38
rogerxaic <roger.miret@gmail.com>
2022-08-22 00:00:54 +0200

    style video player

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -14,17 +14,26 @@ const VideoPlayer: React.FC<{}> = () => {
     startTimestamp: 0,
   });
 
-  const _onVideoStateChange = (data: YouTubeEvent) => {    
-    setState({ ...state,
+  const _onVideoStateChange = (data: YouTubeEvent) => {
+    setState({
+      ...state,
       startTimestamp: data.target.getCurrentTime() * 1000,
-      playing: data.target.getPlayerState() === 1
+      playing: data.target.getPlayerState() === 1,
     });
   };
 
   return (
     <>
-      <YouTube videoId="f-s_Vv82yFw" onStateChange={_onVideoStateChange} />
-      <SubtitlePlayer startTimestamp={state.startTimestamp} playing={state.playing} />
+      <YouTube
+        videoId="f-s_Vv82yFw"
+        style={{ width: "100%", height: "calc(100vh - 230px)" }}
+        opts={{ width: "100%", height: "100%" }}
+        onStateChange={_onVideoStateChange}
+      />
+      <SubtitlePlayer
+        startTimestamp={state.startTimestamp}
+        playing={state.playing}
+      />
     </>
   );
 };
