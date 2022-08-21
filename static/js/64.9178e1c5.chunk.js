c86f1544e94b886ac0f048c0f64f7620516cfedb
ValentinViennot <ValentinViennot@users.noreply.github.com>
2022-08-21 18:58:59 +0000

    deploy: 5ce527559e85df9895adbfaee1310b477705a022

---
+++ static/js/64.9178e1c5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmacraoke=self.webpackChunkmacraoke||[]).push([[64],{64:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});var r=t(837),a=t(184);function u(e){console.log(e.target.getCurrentTime())}var o=function(){return(0,a.jsx)(r.Z,{videoId:"f-s_Vv82yFw",onStateChange:u})}}}]);
+//# sourceMappingURL=64.9178e1c5.chunk.js.map
