b9a0cce9e126994178b6bcec5232bdd9e83af8f5
Valentin Viennot <ValentinViennot@users.noreply.github.com>
2022-08-21 18:24:00 +0000

    add youtube player

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import YouTube from 'react-youtube';
 import './App.css';
+
+function onPlayPause(data: any) {
+  console.log(data.target.getCurrentTime());
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <YouTube
+      videoId='f-s_Vv82yFw'
+      onStateChange={onPlayPause}
+    />
   );
 }
 
