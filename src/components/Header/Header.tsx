194ba9eae26f812f42d42be73cd031e0f9d79c55
rogerxaic <roger.miret@gmail.com>
2022-08-22 17:08:11 +0200

    add "buy me a beer" link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,17 +7,31 @@ interface HeaderProps {}
 const Header: FC<HeaderProps> = () => {
   const a = () => {
     console.log("here");
-  }
+  };
+
+  const beer = () => {
+    window.open("http://buymeacoffee.com/Macraoke", "_blank");
+  };
 
   return (
     <main className="header grid grid-cols-1 mb-5">
       <h1 className="text-3xl font-bold mb-4">Macraoke</h1>
-      <h2 className="text-xl mb-4">Le karaoke avec les discours d'Emmanuel Macron</h2>
+      <h2 className="text-xl mb-4">
+        Le karaoke avec les discours d'Emmanuel Macron
+      </h2>
       <div className="flex flex-row">
-        <button onClick={a} className="mr-3">Random</button>
-        <button onClick={a} className="mr-3">Top 10</button>
-        <button onClick={a} className="mr-3">?</button>
-        <button onClick={a} className="mr-3">Nous offrir une bière</button>
+        <button onClick={a} className="mr-3">
+          Random
+        </button>
+        <button onClick={a} className="mr-3">
+          Top 10
+        </button>
+        <button onClick={a} className="mr-3">
+          ?
+        </button>
+        <button onClick={beer} className="mr-3">
+          🍺 Nous offrir une bière
+        </button>
       </div>
     </main>
   );
