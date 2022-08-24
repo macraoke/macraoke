f049c242cfa96f46f10df06d17be051d7c41093a
Valentin Viennot <ValentinViennot@users.noreply.github.com>
2022-08-24 21:24:51 +0000

    fix extra <br /> not replaced

--- src/components/Subtitle/Subtitle.tsx
+++ src/components/Subtitle/Subtitle.tsx
@@ -16,7 +16,7 @@ const Subtitle: React.FC<SubtitleProps> = (props) => {
   useEffect(() => {
     if (props.subtext === undefined) return;
     const duration = Math.ceil((props.subtext.end - props.subtext.start) * 1000);
-    const words = props.subtext.text.replace('<br />', ' ').split(' ');
+    const words = props.subtext.text.replaceAll('<br />', ' ').split(' ');
     const averageDuration = duration / words.length;
     setWords(words.map((word, index) => ({
       word, activationDelay: !!props.deactivated ? undefined : index * averageDuration * 0.90
